d94f5bc3addbec0bc255b753279e166911274f16
Pakxe <64801796+pakxe@users.noreply.github.com>
2022-09-26 01:49:20 +0900

    [Silver IV] Title: 덱, Time: 136 ms, Memory: 12348 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+// let [n, ...input] = fs
+//   .readFileSync("example.txt")
+//   .toString()
+//   .trim()
+//   .split("\n");
+let [n, ...input] = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+n = Number(n);
+
+let result = "";
+let arr = [];
+for (let i = 0; i < n; i++) {
+  let line = input[i].split(" ");
+
+  if (line[0] === "push_front") {
+    arr.unshift(Number(line[1]));
+  }
+
+  if (line[0] === "push_back") {
+    arr.push(Number(line[1]));
+  }
+
+  if (line[0] === "pop_front") {
+    if (arr.length === 0) result += `-1\n`;
+    else {
+      let top = arr.shift();
+      result += `${top}\n`;
+    }
+  }
+
+  if (line[0] === "pop_back") {
+    if (arr.length === 0) result += `-1\n`;
+    else {
+      let top = arr.pop();
+      result += `${top}\n`;
+    }
+  }
+
+  if (line[0] === "size") {
+    result += `${arr.length}\n`;
+  }
+
+  if (line[0] === "empty") {
+    if (arr.length === 0) result += `1\n`;
+    else result += `0\n`;
+  }
+
+  if (line[0] === "front") {
+    if (arr.length === 0) result += `-1\n`;
+    else {
+      result += `${Number(arr[0])}\n`;
+    }
+  }
+
+  if (line[0] === "back") {
+    if (arr.length === 0) result += `-1\n`;
+    else {
+      result += `${Number(arr[arr.length - 1])}\n`;
+    }
+  }
+}
+
+console.log(result);

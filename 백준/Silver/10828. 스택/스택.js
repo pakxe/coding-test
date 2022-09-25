400bc5cf6a4ba8b8f19d15e9d125d67b01b6eba4
Pakxe <64801796+pakxe@users.noreply.github.com>
2022-09-26 01:29:42 +0900

    [Silver IV] Title: 스택, Time: 140 ms, Memory: 12312 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+// let [n, ...input] = fs
+//   .readFileSync("example.txt")
+//   .toString()
+//   .trim()
+//   .split("\n");
+let [n, ...input] = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+n = Number(n);
+
+let result = "";
+let arr = [];
+for (let i = 0; i < n; i++) {
+  let line = input[i].split(" ");
+
+  if (line[0] === "push") {
+    arr.push(Number(line[1]));
+  }
+
+  if (line[0] === "pop") {
+    if (arr.length === 0) result += `-1\n`;
+    else {
+      let top = arr.pop();
+      result += `${top}\n`;
+    }
+  }
+
+  if (line[0] === "size") {
+    result += `${arr.length}\n`;
+  }
+
+  if (line[0] === "empty") {
+    if (arr.length === 0) result += `1\n`;
+    else result += `0\n`;
+  }
+
+  if (line[0] === "top") {
+    if (arr.length === 0) result += `-1\n`;
+    else {
+      result += `${Number(arr[arr.length - 1])}\n`;
+    }
+  }
+}
+
+console.log(result);

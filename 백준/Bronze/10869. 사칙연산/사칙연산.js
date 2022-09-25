5ac4eec4a97db2e1c68461f595be51c2eab564b0
Pakxe <64801796+pakxe@users.noreply.github.com>
2022-09-26 02:02:45 +0900

    [Bronze V] Title: 사칙연산, Time: 124 ms, Memory: 9608 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let [a, b] = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);

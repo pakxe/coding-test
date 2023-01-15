4ed74b20429db7aed5d331c44e5dc938d5632421
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-01-16 01:28:19 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 1.62 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,35 +1,4 @@
-const MONTH = 28;
-
 function solution(today, terms, privacies) {
-  const list = [];
-  privacies.forEach((person, i) => {
-    const [date, term] = person.split(' ');
-    const t = Number(
-      terms.reduce((l, te) => {
-        const [tt, m] = te.split(' ');
-        if (tt === term) l.push(m);
-        return l;
-      }, []),
-    );
-
-    if (addMonth(t, date) <= Number(today.split('.').join(''))) {
-      list.push(i + 1);
-    }
-  });
-  return list;
+    var answer = [];
+    return answer;
 }
-
-const addMonth = (terms, date) => {
-  let [year, month, day] = date.split('.');
-  year = Number(year) + Math.floor(terms / 12);
-
-  if (Number(month) + (terms % 12) > 12) {
-    year += 1;
-    month = (terms % 12) + Number(month) - 12;
-    month = month < 10 ? `0${month}` : month;
-  } else {
-    month = Number(month) + (terms % 12);
-    month = month < 10 ? `0${month}` : month;
-  }
-  return Number([year, month, day].join(''));
-};

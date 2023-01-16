1415f9011bc2f1e032571756ebc82994d7e2f9a8
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-01-17 02:59:56 +0900

    [level 1] Title: 삼총사, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,28 +1,12 @@
 function solution(number) {
-    const a = [];
-    const arr = new Array(number.length).fill(0).map(_ => new Array(number.length).fill(0));
-    let triple = 0;
-    for(let i = 0; i < number.length - 1; i++){
-        for(let j = i + 1; j < number.length; j++) {
-            const sum2 = number[i] + number[j];
-            const matchCount = number.filter((n, index) => index!==i && index!==j && n===(0-sum2)).length;
-            if(matchCount) triple+= matchCount;
+    let trio = 0;
+    for(let i = 0; i < number.length - 2; i++){
+        for(let j = i + 1; j < number.length - 1; j++) {
+            for(let k = j + 1; k < number.length; k++) {
+                if(!(number[i] + number[j] + number[k])) trio++;
+            }
         }
     }
-    return triple ? triple/3 : 0;
-    //아 근데 같은 값 갖는 친구도 있다매
-    // 자기 자신을 제외해야함
+    return trio;
 }
 
-/*
-[x] 2개 끼리 더한 배열을 만든다
-[ ] 만든 배열을 똑같이 순회한다. 
-[ ] 0 - (이때 가져온값) 이 number에 있다면 삼총사 수를 1늘린다.
-*/
-
-// 12 13 14 15
-//    23 24 25
-//       34 35
-//          45
-// j : 1 2 3 4..
-// j : 2345 345 45 5???? 이건 for

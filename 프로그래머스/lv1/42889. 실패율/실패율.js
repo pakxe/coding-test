c197dea9f693c0b67f0787864d42b72481c7ce45
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-02-07 23:15:26 +0900

    [level 1] Title: 실패율, Time: 0.21 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -0,0 +1,24 @@
+function solution(N, stages) {
+  const sortedUser = stages.sort((a, b) => a - b); // 오름차순 정렬
+  const failureRates = new Array(N).fill(0);
+
+  for (let i = 1; i <= N; i++) {
+    const failureUsers = sortedUser.indexOf(i) !== -1 ? sortedUser.lastIndexOf(i) - sortedUser.indexOf(i) + 1 : 0;
+    const upperUsers = sortedUser.indexOf(i) !== -1 ? sortedUser.length - sortedUser.indexOf(i) : 0;
+
+    failureRates[i - 1] = failureUsers ? failureUsers / upperUsers : 0;
+  }
+  console.log(failureRates);
+  return failureRates
+    .map((_, index) => index)
+    .sort((a, b) => failureRates[b] - failureRates[a])
+    .map((a) => a + 1);
+}
+
+console.log(solution(4, [4, 4, 4, 4, 4]));
+/**
+ * 1. 몇명이 해당 스테이지에 있는지 확인한다.(클리어하지 못했는지)
+ * 2. 몇명이 해당 스테이지를 통과했는지 확인한다.
+ */
+
+

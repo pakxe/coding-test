cce257a84ccee291643e8c52f9eac5033b9e0c96
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-03-13 00:52:00 +0900

    [level 2] Title: 피보나치 수, Time: 2.92 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -1,12 +1,15 @@
-function getFibonacci(n) {
-    let fNum = [0, 1, 1];
-    for(let i = 3; i <= n; i++) {
-      fNum[i] = (fNum[i - 1] + fNum[i - 2])%1234567; 
+const fibo = n => {
+    const arr = new Array(100001).fill(0);
+    arr[0] = 0;
+    arr[1] = 1;
+    
+    for(let i = 2; i <= n; i++) {
+        arr[i] = (arr[i-1] % 1234567) + (arr[i-2] % 1234567)
     }
-    return fNum[n];
+    
+    return arr[n] %1234567
 }
 
 function solution(n) {
-    const answer = getFibonacci(n) % 1234567;
-    return answer;
+    return fibo(n);
 }

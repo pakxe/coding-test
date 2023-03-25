7a80e1483b6c710c0f17ca317c495484374a33ac
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-03-25 23:00:53 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,15 @@
+function solution(n,a,b){
+    let count = 0;
+    
+    while(a !== b) {
+        a = Math.ceil(a/2);
+        b = Math.ceil(b/2);
+        count++;
+    }
+    
+    return count;
+}
+
+/*
+4 7 -> 1/ 2, 4 -> 2/ 1, 2-> 3/ 1, 1
+*/

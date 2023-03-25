0d36d68de9a572037fb1269b9518ea1f2f151355
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-03-26 00:45:48 +0900

    [level 2] Title: H-Index, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -2,10 +2,14 @@ function solution(citations) {
     let i = 0; // i 이상인 논문의 개수
     
     for(i = citations.length; i >= 0; i--) {
-        const h = citations.filter(c => c >= i).length;
-        if(h >= i) break;
+        const i이상인논문의개수 = citations.filter(c => c >= i).length;
+        if(i이상인논문의개수 >= i) break;
     }
     
     return i;
 }
+
+/*
+i이상인 논문의 개수를 센다. 이 개수는 
+*/
 

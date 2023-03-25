f174a2bddbc33c6a03296f43656f14518b932502
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-03-25 23:41:27 +0900

    [level 2] Title: N개의 최소공배수, Time: 302.87 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -0,0 +1,17 @@
+function solution(arr) {
+    return findLeastCommonMultiple(arr);
+}
+
+
+const findLeastCommonMultiple = (arr) => {
+    let i = 1;
+    
+    while(1) {
+        const count = arr.filter(a => i % a === 0).length;
+        if(count === arr.length) break;
+        else i++;
+    }
+    
+    return i;
+}
+

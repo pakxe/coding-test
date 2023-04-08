47c707fbb02b6234f316fa8f3637ea61075fea88
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-04-09 07:02:48 +0900

    [level 2] Title: 오픈채팅방, Time: 125.27 ms, Memory: 99.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
@@ -0,0 +1,40 @@
+const [ENTER, LEAVE, CHANGE] = ['Enter', 'Leave', 'Change'];
+const [IS_ENTER, IS_LEAVE] = [1, 0];
+const [WELCOME, GOODBYE] = ['님이 들어왔습니다.', '님이 나갔습니다.'];
+
+function solution(record) {
+    const map = {};
+    const result = [];
+    
+    record.forEach((line) => {
+        const [command, userId, nickName] = line.split(' ');
+        
+        if(command === ENTER) {
+            map[userId] = nickName;
+            result.push([IS_ENTER, userId]);
+        }
+        else if(command === LEAVE) {
+            result.push([IS_LEAVE, userId]);   
+        }
+        else {
+            map[userId] = nickName;
+        }
+    })
+    
+    return result.map(([type, userId]) => {
+        if(type === IS_ENTER) return map[userId] + WELCOME;
+        if(type === IS_LEAVE) return map[userId] + GOODBYE;
+    })
+}
+
+/*
+어차피 배열은 두번 읽어야한다. 
+유저아이디를 키값으로 하는 맵을 만들자.
+
+배열을 순회한다.
+enter면 출력배열에 추가하고, 아이디를 키갑승로 닉네임을 밸류로하는 셋을 추가한다. 
+leave면 출력배열에 추가한다. 
+change면 아이디를 키갑승로 닉네임을 밸류로하는 셋을 추가한다.
+
+배열을 쭉 읽으면서.. 출력 배열을 만드는데 [1, id] [0, id] 형태로하자. 1은 들어온거고 2는 나가는거다. 
+*/

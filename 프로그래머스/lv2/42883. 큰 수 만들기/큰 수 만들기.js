ecf1db41e5d4250910fa69c5326c19dcbdd5a1b9
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-04-26 02:03:06 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,28 +1,44 @@
-const solution = (number, k) => {
+function solution(number, k) {
     const stack = [];
+    let deleteCount = 0;
+    let count = 0;
     
-    for(let i = 0 ; i < number.length; i++) {
-        const num = Number(number[i]);
-        
-        if(stack.length === 0) {
-            stack.push(num);
+    stack.push(number[0]);
+    
+    for(let i = 1; i < number.length; i++) {
+        if(deleteCount === k) {
+                count = i;
+                break;
+            }
+        while(stack.length !== 0 && stack[stack.length - 1] < number[i]) {
+            if(deleteCount === k) {
+                count = i;
+                break;
+            }
             
-            continue;
+            stack.pop();
+            deleteCount++;
         }
         
-        while(stack[stack.length - 1] < num) { // 현재 숫자보다 작은 숫자들 제거       
-            if(k === 0) break; // 더이상 제거할 수 없거나
-            if(stack.length === 0) break; // 스택이 비어있다면 중지
-            stack.pop();
-            k--;
-        }
-        stack.push(num);
+        stack.push(number[i]);
     }
+    
+    if(stack.length !== number.length - k) {
+        for(let i = count; i < number.length; i++){
+            stack.push(number[i]);
+        }
+    }
+    
     
     return stack.join('').substr(0, number.length - k);
 }
 
 /*
-결국 큰 수를 스택에 남기는 방법이다. 이때 최대 
+숫자를 읽으면서 더 제거할 수 있으면 제거한다. 
+while을 돌면서 현재보다 더 작은 수들을 꺼낸다. 
+
+다 꺼냈으면 현재 수를 집어넣는다. 
+
+만약 현재가 더 작은 수라면 그냥 push한다. 어차피 제거될테니까.
 */
 

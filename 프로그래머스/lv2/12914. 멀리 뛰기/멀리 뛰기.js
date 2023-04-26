cd1918a88f569ed46c246bfc023353ed7060b416
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-04-26 18:36:12 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.25 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -3,13 +3,13 @@ function solution(n) {
 }
 
 const jump = n => {
-    const dp = new Array(n).fill(0);
+    const dp = new Array(n).fill(0); // n개의 길이 배열 dp 생성
     
     dp[1] = 1; // 1칸
     dp[2] = 2; // 1칸1칸, 2칸
     
     for(let i = 3; i <= n; i++) {
-        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567; // jump(n) = jump(n-1) + jump(n-2)
     }
     
     return dp[n];

309379597163ef09e58b86a72561a14a96881a2b
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-05-01 01:49:00 +0900

    [level 2] Title: 숫자의 표현, Time: 2.42 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,20 +1,21 @@
 function solution(n) {
-    let count = 0;
-    const stack = [];
-    let sum = 0;
+    let answer = 0;
+    let N = 1;
     
-    for(let i = 1; i <= n + 1; i++) {
-        if(sum === n) count += 1;
-        
-        stack.push(i);
-        sum += i;
-        
-        while(sum > n) {
-            const deletedNum = stack.shift();
-            sum -= deletedNum;
-        }
+    while( n > 0 ) {
+        n = n - N;
+        if(!(n % N)) answer++;
+        N++;
     }
     
-    return count;
+    return answer;
 }
 
+/*
+    n이 자연수면 연속된 자연수의 합으로 표현이 가능하다는 의미
+    1 부터 시작 -> 1 + 1n = 15, 1n = 14, n = 14
+    2 -> 1 + 2 + 2n = 15, 2n = 12, n = 6
+    3 -> 1 + 2 + 3 + 3n = 15, 3n = 9, n = 3
+    ...
+    
+*/

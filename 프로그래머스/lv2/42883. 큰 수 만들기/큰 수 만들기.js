9c2b86c4725b8570f97b493b526cb193f4aeed39
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-05-03 14:42:42 +0900

    [level 2] Title: 큰 수 만들기, Time: 2.91 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,44 +1,26 @@
 function solution(number, k) {
     const stack = [];
     let deleteCount = 0;
-    let count = 0;
-    
     stack.push(number[0]);
+    console.log(stack);
     
     for(let i = 1; i < number.length; i++) {
-        if(deleteCount === k) {
-                count = i;
-                break;
-            }
-        while(stack.length !== 0 && stack[stack.length - 1] < number[i]) {
-            if(deleteCount === k) {
-                count = i;
-                break;
-            }
+        while(deleteCount < k && stack[stack.length - 1] < number[i]) {
+            if(stack.length === 0) break;
             
-            stack.pop();
             deleteCount++;
+            stack.pop();
         }
-        
         stack.push(number[i]);
     }
     
-    if(stack.length !== number.length - k) {
-        for(let i = count; i < number.length; i++){
-            stack.push(number[i]);
-        }
-    }
-    
-    
-    return stack.join('').substr(0, number.length - k);
+    return stack.join('').slice(0, number.length - k);
 }
 
 /*
-숫자를 읽으면서 더 제거할 수 있으면 제거한다. 
-while을 돌면서 현재보다 더 작은 수들을 꺼낸다. 
-
-다 꺼냈으면 현재 수를 집어넣는다. 
-
-만약 현재가 더 작은 수라면 그냥 push한다. 어차피 제거될테니까.
+    k개의 수를 제거해서 만드는 것은 n - k 가 최종 자리수라는 뜻이다. 
+    제거할 수 있는 수는 k 개까지.
+    k개를 제거했다면 더이상 제거할 수 없으므로 return 한다. 
+    최대수를 만드는 것은 현재와 다음 수를 비교해서 다음 수가 더 크면 현재수를 빼버리고 다음 수를 넣으면 된다. 
+    빼는건 k까지 가능
 */
-

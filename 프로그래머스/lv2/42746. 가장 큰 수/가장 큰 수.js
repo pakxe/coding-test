029c868b7a33a5959f21ee75cb68605a1590a67f
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-05-05 14:32:09 +0900

    [level 2] Title: 가장 큰 수, Time: 0.08 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -2,30 +2,28 @@ function solution(numbers) {
     const stack = [];
     const strNums = numbers.sort().reverse().map(n => n.toString());
     
-    stack.push(strNums[0]);
-    
+    stack.push(strNums[0]); // 초기값
     
     for(let i = 1; i < numbers.length; i++) {
-        const temp = [];
+        const temp = []; // 임시로 저장할 변수
         
-        // stack이 비어있을 떄도 판단해야한다.
+        // 교환한 수가 더 크다면 교환
         while(parseInt(stack[stack.length - 1] + strNums[i]) < parseInt(strNums[i] + stack[stack.length - 1])){
-            temp.push(stack.pop());
-            if(stack.length === 0) break;
+            temp.push(stack.pop()); // 임시로 저장할 변수에 교환된 수를 넣는다.
+            if(stack.length === 0) break; // 스택 길이가 0이면 더이상 pop할 수 없으므로 break
         }
         
-        stack.push(strNums[i]);
-        if(temp.length !== 0) {
-            
-            while(temp.length !== 0) {
-                stack.push(temp.pop());
+        stack.push(strNums[i]); // 스택에 현재 수를 push
+        if(temp.length !== 0) { 
+            while(temp.length !== 0) { // 임시로 저장한 변수의 내부가 빌 때까지 반복
+                stack.push(temp.pop()); // 스택의 끝에 pop했던 수들을 넣는다.
             }
         }
         
     }
     
-    let result = stack.join('');
-    return parseInt(result) === 0 ? '0' : result;
+    let result = stack.join(''); 
+    return parseInt(result) === 0 ? '0' : result; // '0000' = '0' 이므로 예외처리
 }
 
 

ae6c2343f969c2bad25f48a23b04df782c693d18
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-05-12 03:22:19 +0900

    [level 2] Title: [3차] n진수 게임, Time: 0.82 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -1,41 +1,19 @@
 function solution(n, t, m, p) {
-    let need = p; // 필요 칸 수 본인포함
-    const result = [];
-    let leftCount = 0;
+    let subNum = '';
+    let i = 0;
+    let result = '';
     
-    for(let i = 0; i < t * m; i++) {
-        let number = i.toString(n);
-        while(1) {
-            if(leftCount===t) break;
-            if(number.length < need) {
-                need -= number.length;
-                break;
-            }
-            if(number.length >= need) {
-                result.push(number[need-1]);
-                leftCount++;
-                
-                if(need === number.length) {
-                    need = m;
-                    break;
-                }
-                else {
-                    number = number.slice(need);
-                    need = m;
-                }
-            }
+    while(result.length < t) {
+        if(subNum.length < m) {
+            subNum += i.toString(n);
+            i++;
         }
-        
+        else {
+            // 추가할 수 있음
+            result += subNum[p-1];
+            subNum = subNum.slice(m);
+        }
     }
     
-    return result.join('').toUpperCase();
+    return result.toUpperCase()
 }
-
-/*
-    숫자 외치기
-    남은 카운트 ++
-    jump 갱신하기
-    만약 제일 끝 출력했다면, continue
-    아니라면 다음부터 배열 끊고 jump그대로
-    
-*/

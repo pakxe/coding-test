1936e87706258619e29d9fcd21f3caf2104feb1a
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-09-05 01:30:17 +0900

    [Silver IV] Title: 괄호, Time: 140 ms, Memory: 9528 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.js
+++ 백준/Silver/9012. 괄호/괄호.js
@@ -1,34 +1,27 @@
-const fs = require("fs");
-let [n, ...arr] = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const fs = require('fs');
+// let input = fs.readFileSync('e.txt').toString().trim().split('\n');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const LINE_COUNT = parseInt(input.shift());
 
-let stack = [];
-let stop = false;
-let result = false;
-let con = "";
-for (let i = 0; i < n; i++) {
-  // 문장들
-  stop = false;
-  stack = []; // 스택 비우기
-  let str = arr[i];
-  for (let j = 0; j < str.length; j++) {
-    // 문장 내부
-    if (str[j] === "(") {
-      // 열린 괄호
-      stack.push(1);
-    } else {
-      // 닫힌 괄호
-      if (stack.length === 0) {
-        // 스택이 비어있다면
-        stop = true;
-        break;
-      } else {
-        // 안비어있다면
-        stack.pop();
-      }
-    }
-  }
-  if (stack.length !== 0 || stop === true) console.log("NO");
-  else console.log("YES");
-}
+const OPEN = '(';
+const CLOSE = ')';
 
-// 스택이 안비어있으면 false, 또는 스택이 비어있으면서
+const SUCCESS = 'YES';
+const FAIL = 'NO';
+
+const isValidPS = (str) => {
+	const stack = [];
+
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] === OPEN) stack.push(str[i]);
+		if (str[i] === CLOSE) {
+			if (stack.length === 0) return false;
+			else stack.pop();
+		}
+	}
+
+	return stack.length !== 0 ? false : true;
+};
+
+for (let i = 0; i < LINE_COUNT; i++)
+	console.log(isValidPS(input[i]) ? SUCCESS : FAIL);

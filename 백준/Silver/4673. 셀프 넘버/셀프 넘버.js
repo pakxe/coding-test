cf89b784cdf10bd5af622650d9c0cbb680c60433
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-09-05 13:51:27 +0900

    [Silver V] Title: 셀프 넘버, Time: 180 ms, Memory: 10696 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.js
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.js
@@ -1,21 +1,34 @@
-const num = 10001;
-let arr = new Array(num);
-arr.fill(0); //0으로 초기화
+const fs = require('fs');
+// let input = fs.readFileSync('e.txt').toString().trim().split('\n');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// const LINE_COUNT = parseInt(input.shift());
 
-let sum = 0;
-let str = "";
+/**
+ * [] 10000 번 반복을 돌려서 생성자가 있는 숫자는 제거한다.
+ */
 
-for (let i = 0; i < num; i++) {
-  sum = i;
+const LIMIT = 10000;
 
-  str = i.toString(); //숫자를 문자로 바꾼 것
-  for (let j = 0; j < str.length; j++) {
-    sum += Number(str[j]);
-  }
-  if (sum > 10000) continue;
-  arr[sum]++;
+const getStrNumSum = (num) => {
+	let sum = num;
+	const strNum = num.toString();
+
+	for (let i = 0; i < strNum.length; i++) {
+		sum += parseInt(strNum[i]);
+	}
+
+	return sum;
+};
+
+const selfNumArr = new Array(LIMIT + 1).fill(false);
+
+for (let i = 1; i <= LIMIT; i++) {
+	selfNumArr[getStrNumSum(i)] = true;
 }
 
-for (let i = 0; i < num; i++) {
-  if (arr[i] === 0) console.log(i);
+const res = [];
+for (let i = 1; i <= LIMIT; i++) {
+	if (!selfNumArr[i]) res.push(i);
 }
+
+console.log(res.join('\n'));

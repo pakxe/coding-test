288d1051d5fab873bac0a4ca2ed6161726d9c6c0
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-09-09 03:58:24 +0900

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.js
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -1,13 +1,7 @@
 const fs = require('fs');
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n").map(Number);
+//let input = fs.readFileSync('e.txt').toString().split('\n');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-let max = Math.max(...input);
-let where = 0;
-
-for (let i = 0; i < input.length; i++) {
-  if (max === input[i]) {
-    where = i + 1;
-  }
-}
+const max = Math.max(...input.map(Number));
 console.log(max);
-console.log(where);
+console.log(input.map(Number).indexOf(max) + 1);

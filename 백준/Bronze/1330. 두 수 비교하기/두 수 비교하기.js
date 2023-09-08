0d07b815e799c7c0daf9cb02dc7ea9028fdf8e23
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-09-09 03:54:07 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 124 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
@@ -1,9 +1,8 @@
-let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().split(" ").map(Number);
-// let input = fs.readFileSync("example.txt").toString().split(" ").map(Number);
+const fs = require('fs');
+//let input = fs.readFileSync('e.txt').toString().split('\n');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const [a, b] = input[0].split(' ').map(Number);
 
-const A = input[0];
-const B = input[1];
-if (A > B) console.log(">");
-else if (A < B) console.log("<");
-else if (A == B) console.log("==");
+if(a > b) console.log('>');
+if(a < b) console.log('<');
+if(a === b) console.log('==');

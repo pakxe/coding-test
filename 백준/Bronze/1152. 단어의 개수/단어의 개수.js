20e0b7dbc593d5a2367dcbc66dc5fa62ac3114a3
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-09-09 03:52:31 +0900

    [Bronze II] Title: 단어의 개수, Time: 156 ms, Memory: 17352 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.js
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -1,5 +1,7 @@
-let fs = require("fs");
-let input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+const fs = require('fs');
+//let input = fs.readFileSync('e.txt').toString().split('\n');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// const [a, b] = input[0].split(' ').map(Number);
 
-if (input[0] == "") console.log(0);
-else console.log(input.length);
+if (input[0] === ' ' || input[0] === '') console.log(0);
+else console.log(input[0].trim().split(' ').length);

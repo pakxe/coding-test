ef47a864d2dca6807750a59172a77e7140d752ee
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-09-11 17:41:42 +0900

    [Silver I] Title: 곱셈, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.js
+++ 백준/Silver/1629. 곱셈/곱셈.js
@@ -1,18 +1,24 @@
-const fs = require("fs");
-const [a, b, c] = fs
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split(" ")
-  .map(BigInt);
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const [A, B, C] = input[0].split(' ').map(BigInt);
 
-function power(a, n) {
-  if (n === BigInt(0)) return BigInt(1);
+/*
+ 수의 overflow가 일어낳 가능성이 크다.
 
-  let x = power(a, BigInt(parseInt(n / BigInt(2))));
+ 2^31 = 문제에서 제공한 수의 범위
+*/
 
-  if (n % BigInt(2) === BigInt(0)) return ((x % c) * (x % c)) % c;
-  else return ((x % c) * ((x * a) % c)) % c;
-}
+const f = (a, b, c) => {
+	if (b === BigInt(1)) return a % c;
 
-console.log(power(a, b).toString());
+	if (b % BigInt(2) === BigInt(0)) {
+		// b가 짝수인 경우
+		return f(a, b / BigInt(2), c) ** BigInt(2) % c;
+	} else {
+		return (f(a, (b - BigInt(1)) / BigInt(2), c) ** BigInt(2) * a) % c;
+	}
+};
+
+const answer = f(A, B, C).toString();
+
+console.log(answer);

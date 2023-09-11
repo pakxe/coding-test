2948a4cb9de262e006316efdb672b206a30ee7bb
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-09-12 00:13:45 +0900

    [level 2] Title: H-Index, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -2,14 +2,16 @@ function solution(citations) {
     let i = 0; // i 이상인 논문의 개수
     
     for(i = citations.length; i >= 0; i--) {
-        const i이상인논문의개수 = citations.filter(c => c >= i).length;
-        if(i이상인논문의개수 >= i) break;
+        const overI = citations.filter(c => c >= i).length;
+        if(overI >= i) break;
     }
     
     return i;
 }
 
 /*
-i이상인 논문의 개수를 센다. 이 개수는 
+i이상인 논문의 개수를 센다. 이 개수는 overI라고 한다. 
+이 overI가 i 보다 크면 멈춘다. 그게 조건이니까
+
 */
 

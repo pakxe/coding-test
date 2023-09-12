5cfb841f674af794ec02f63388edb677908f1280
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-09-12 22:56:41 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 660 ms, Memory: 69524 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -1,29 +1,24 @@
-const fs = require("fs");
-const [n, ...points] = fs
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split("\n");
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const length = parseInt(input[0]);
 
-points.sort(function (a, b) {
-  let na = a.split(" ").map(Number);
-  let nb = b.split(" ").map(Number);
-  if (na[0] < nb[0]) return -1;
-  if (na[0] < nb[0]) return 1;
-  if (na[0] === nb[0]) {
-    if (na[1] < nb[1]) return -1;
-    else return 1;
-  }
-});
+const coordinates = [];
 
-let str = "";
-points.forEach((e) => {
-  str += e;
-  str += "\n";
-});
+for (let i = 1; i < input.length; i++) {
+	coordinates.push(input[i].split(' ').map(Number));
+}
 
-console.log(str);
+const customSort = (a, b) => {
+	const [ax, ay] = a;
+	const [bx, by] = b;
 
-//이 함수가 리턴하는 값이 0보다 작을 경우,  a가 b보다 앞에 오도록 정렬하고,
-// 이 함수가 리턴하는 값이 0보다 클 경우, b가 a보다 앞에 오도록 정렬합니다.
-// 만약 0을 리턴하면, a와 b의 순서를 변경하지 않습니다.
+	if (ax === bx) return ay - by;
+	else return ax - bx;
+};
+
+console.log(
+	coordinates
+		.sort(customSort)
+		.map((coordinate) => coordinate.join(' '))
+		.join('\n')
+);

f699416358ec999995c29f9c77b7bea980349acd
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-09-18 02:24:09 +0900

    [Bronze I] Title: 평균, Time: 120 ms, Memory: 9604 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.js
+++ 백준/Bronze/1546. 평균/평균.js
@@ -1,24 +1,9 @@
-const fs = require("fs");
-const [n, ...arr] = fs
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split(/\s/)
-  .map(Number);
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-let max = 0; //최고 점수을 저장해줄 변수
-arr.forEach((e) => {
-  if(e >= max) max = e;
-})
+const count = parseInt(input[0]);
+const subs = input[1].split(' ').map(Number);
 
-let newScore = []; //조작한 점수를 담아줄 배열
-arr.forEach((e)=> {
-    newScore.push(e/max*100);
-})
+const max = Math.max(...subs);
 
-let sum = 0;
-newScore.forEach((e) => {
-  sum += e;
-})
-
-console.log(sum / n);
+console.log(subs.reduce((sum, cost) => sum + (cost / max) * 100, 0) / count);

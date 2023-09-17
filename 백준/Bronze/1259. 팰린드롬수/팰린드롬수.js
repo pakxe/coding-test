fb4d3495bb0373e0ab28c80586b58d06e24d48e4
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-09-18 03:49:53 +0900

    [Bronze I] Title: 팰린드롬수, Time: 120 ms, Memory: 9320 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.js
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.js
@@ -1,16 +1,20 @@
-const fs = require("fs");
-const arr = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-arr.pop();
-let isF = false;
+const res = [];
 
-for (let i = 0; i < arr.length; i++) {
-  isF = true;
-  let str = arr[i];
-  for (let j = 0; j < Math.floor(str.length / 2); j++) {
-    if (str[j] !== str[str.length - j - 1]) isF = false;
-  }
+const getRes = (str) => {
+	for (let j = 0; j < Math.floor(str.length / 2); j++) {
+		if (str[j] !== str[str.length - 1 - j]) {
+			return 'no';
+		}
+	}
 
-  if (isF) console.log("yes");
-  else console.log("no");
+	return 'yes';
+};
+
+for (let i = 0; i < input.length - 1; i++) {
+	res.push(getRes(input[i].toString()));
 }
+
+console.log(res.join('\n'));

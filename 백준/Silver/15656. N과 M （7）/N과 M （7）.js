2c41dd6864e456d2c24411d92105615f34827116
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-09-24 03:58:12 +0900

    [Silver III] Title: N과 M (7), Time: 3760 ms, Memory: 605664 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+// const input = fs.readFileSync('e.txt').toString().trim().split('\n');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [N, M] = input[0].split(' ').map(Number);
+const arr = input[1]
+	.split(' ')
+	.map(Number)
+	.sort((a, b) => a - b);
+
+// 중복 x 순서 상관 있음. M 길이의 수열
+// 사전 순으로 증가해야한다는 것은 순서가 상관 없다는 의미다.
+
+/*
+이어서 읽어야할 인덱스를 준다. 그리고 현재 길이
+*/
+
+const getSequence = (arr, lest) => {
+	// 길이를 넘으면 멈춘다.
+	if (lest === 1) return arr.map((el) => [el]);
+
+	const result = [];
+
+	arr.forEach((cur, index) => {
+		const rest = arr;
+		const sequence = getSequence(rest, lest - 1);
+		result.push(...sequence.map((el) => [cur, ...el]));
+	});
+
+	return result;
+};
+
+// const arr = new Array(N).fill().map((_, i) => i + 1);
+
+console.log(
+	getSequence(arr, M)
+		.map((arr) => arr.join(' '))
+		.join('\n')
+);

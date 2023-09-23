984a3bfa35af2980d16d32ef5ab4632640c2123d
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-09-24 07:24:15 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 136 ms, Memory: 9380 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.js
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.js
@@ -1,8 +1,15 @@
-let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString();
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// const input = fs.readFileSync('e.txt').toString().trim().split('\n');
+const n = parseInt(input[0]);
 
-let n = Number(input);
+const result = [];
 
 for (let i = 1; i <= n; i++) {
-  console.log(" ".repeat(n - i) + "*".repeat(i));
+	const empty = ' '.repeat(n - i);
+	const star = '*'.repeat(i);
+
+	result.push(empty + star);
 }
+
+console.log(result.join('\n'));

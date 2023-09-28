535fbaac3f842328a81f094fdfc84e1588d5602f
Pakxe <64801796+pakxe@users.noreply.github.com>
2023-09-29 03:28:20 +0900

    [Silver I] Title: 단지번호붙이기, Time: 168 ms, Memory: 10752 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
@@ -1,53 +1,98 @@
-const bfs = (graph, start) => {
-  const dx = [0, 1, 0, -1];
-  const dy = [1, 0, -1, 0];
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'e.txt';
+let input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+const row = parseInt(input.shift());
 
-  const needVisit = [];
+input = input.map((row) => row.split('').map(Number));
 
-  let houseCount = 0;
-
-  needVisit.push(start);
-
-  while (needVisit.length !== 0) {
-    const node = needVisit.shift(); // 0, 1
-
-    // 방문하지 않은 노드라면
-    if (graph[node[0]][node[1]] !== 0) {
-      houseCount++;
-      graph[node[0]][node[1]] = 0; // 방문
-
-      for (let i = 0; i < dx.length; i++) {
-        const ny = node[0] + dy[i];
-        const nx = node[1] + dx[i];
-
-        if (nx >= 0 && ny >= 0 && nx <= graph[0].length - 1 && ny <= graph.length - 1 && graph[ny][nx] !== 0) {
-          needVisit.push([ny, nx]);
-        }
-      }
-    }
-  }
-
-  return houseCount;
-};
-
-const fs = require('fs');
-// let input = fs.readFileSync('./input.txt').toString().split('\n');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-const size = parseInt(input[0]);
-input.shift();
-
-const map = input.map((m) => m.split('').map(Number));
-
-const houseCount = [];
-
-for (let i = 0; i < size; i++) {
-  for (let j = 0; j < size; j++) {
-    if (map[i][j] !== 0) {
-      houseCount.push(bfs(map, [i, j]));
-    }
-  }
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
 }
 
-houseCount.sort((a, b) => a - b)
-console.log(houseCount.length);
-console.log(houseCount.reduce((total, count) => total + count + '\n', ''));
+class Queue {
+	#size;
+	#head;
+	#tail;
+
+	constructor() {
+		this.#size = 0;
+		this.#head = null;
+		this.#tail = null;
+	}
+
+	push(value) {
+		let node = new Node(value);
+
+		if (this.#size === 0) this.#head = node;
+		else this.#tail.next = node;
+
+		this.#tail = node;
+
+		this.#size++;
+	}
+
+	getSize() {
+		return this.#size;
+	}
+
+	shift() {
+		if (this.#size === 0) return null;
+
+		const value = this.#head.value;
+
+		if (this.#size === 1) {
+			this.#head = null;
+			this.#tail = null;
+		} else this.#head = this.#head.next;
+
+		this.#size--;
+
+		return value;
+	}
+}
+
+const dx = [0, 1, 0, -1];
+const dy = [1, 0, -1, 0];
+
+// 방문 여부는 기존 지도에 0으로 표시
+const bfs = (start) => {
+	const queue = new Queue();
+	let count = 0;
+
+	queue.push(start);
+
+	while (queue.getSize() > 0) {
+		const [y, x] = queue.shift();
+
+		if (input[y][x] !== 0) {
+			input[y][x] = 0; // 방문 표시
+			count++; // 집 수 추기
+
+			for (let i = 0; i < 4; i++) {
+				const [ny, nx] = [y + dy[i], x + dx[i]];
+
+				if (
+					![-1, row].includes(ny) &&
+					![-1, row].includes(nx) &&
+					input[ny][nx] !== 0
+				)
+					queue.push([ny, nx]);
+			}
+		}
+	}
+
+	return count;
+};
+
+const result = [];
+
+for (let y = 0; y < row; y++) {
+	for (let x = 0; x < row; x++) {
+		if (input[y][x] !== 0) result.push(bfs([y, x]));
+	}
+}
+
+console.log(result.length);
+console.log(result.sort((a, b) => a - b).join('\n'));

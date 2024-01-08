d61b5ae629ac705dd2309cb39812353388d6cae5
PAKXE <64801796+pakxe@users.noreply.github.com>
2024-01-09 04:39:44 +0900

    [Silver I] Title: 곱셈, Time: 128 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.js
+++ 백준/Silver/1629. 곱셈/곱셈.js
@@ -1,24 +1,28 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().split('\n');
-const [A, B, C] = input[0].split(' ').map(BigInt);
-
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'e.txt';
+const input = require('fs').readFileSync(filePath).toString().split('\n');
+const [A, B, C] = input.shift().split(' ').map(BigInt);
 /*
- 수의 overflow가 일어낳 가능성이 크다.
+짝수와 홀수인 경우를 생각해야한다.
 
- 2^31 = 문제에서 제공한 수의 범위
+짝수: A^B = A^(B/2) * A^(B/2)
+A^B % C = (A^(B/2) * A^(B/2)) % C
+        = (A^(B/2) % C * A^(B/2) % C) % C;
+        = f(B/2)^2 % C
+
+홀수: A ^ B = (A^(B/2) * A^(B/2) * A
+A ^ B % C = (A^(B/2) * A^(B/2) * A) % C
+          = ((A^(B/2) * A^(B/2)) % C * A % C) % C
+          = (f(B/2)^2 % C * A % C) % C
+          = f(B/2)^2 * A % C
 */
 
-const f = (a, b, c) => {
-	if (b === BigInt(1)) return a % c;
+// 모든 인자는 BigInt
+function modular(a, b, c) {
+  if (b <= BigInt(1)) return a % c;
 
-	if (b % BigInt(2) === BigInt(0)) {
-		// b가 짝수인 경우
-		return f(a, b / BigInt(2), c) ** BigInt(2) % c;
-	} else {
-		return (f(a, (b - BigInt(1)) / BigInt(2), c) ** BigInt(2) * a) % c;
-	}
-};
+  // 짝수인 경우
+  if (b % BigInt(2) === BigInt(0)) return modular(a, b / BigInt(2), c) ** BigInt(2) % c;
+  else return (modular(a, (b - BigInt(1)) / BigInt(2), c) ** BigInt(2) * A) % c;
+}
 
-const answer = f(A, B, C).toString();
-
-console.log(answer);
+console.log(modular(A, B, C).toString());

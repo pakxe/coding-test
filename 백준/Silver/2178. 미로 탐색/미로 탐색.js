ee9dc8a9b5061a61751fbf58d4caf80e01c4d8c3
PAKXE <64801796+pakxe@users.noreply.github.com>
2024-01-15 00:37:15 +0900

    [Silver I] Title: 미로 탐색, Time: 216 ms, Memory: 12876 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.js
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.js
@@ -1,43 +1,113 @@
-const fs = require('fs');
-//let input = fs.readFileSync('e.txt').toString().split('\n');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-const [row, col] = input[0].split(' ').map(Number);
-input.shift();
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'e.txt';
+const input = require('fs').readFileSync(filePath).toString().split('\n');
 
-const WAY = 1;
+const VISITED = -1;
 const WALL = 0;
-const VISITED = 2;
+const INVALID_TILE = [VISITED, WALL];
 
 const DX = [0, 1, 0, -1];
 const DY = [1, 0, -1, 0];
 
+const [row, col] = input.shift().split(' ').map(Number);
 const map = new Array(row).fill();
 
-for (let i = 0; i < input.length; i++) {
-	map[i] = input[i].split('').map(Number);
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
-const bfs = () => {
-	const queue = [];
-	queue.push([0, 0, 1]);
+class Queue {
+  #size;
+  #head;
+  #tail;
 
-	while (queue.length > 0) {
-		const [y, x, walkCount] = queue.shift();
+  constructor() {
+    this.#size = 0;
+    this.#head = null;
+    this.#tail = null;
+  }
 
-		if (map[y][x] !== VISITED) {
-			if (y === row - 1 && x === col - 1) return walkCount;
+  push(value) {
+    const node = new Node(value);
 
-			map[y][x] = VISITED;
-			for (let i = 0; i < DX.length; i++) {
-				if (
-					![-1, row].includes(y + DY[i]) &&
-					![-1, col].includes(x + DX[i]) &&
-					map[y + DY[i]][x + DX[i]] === WAY
-				)
-					queue.push([y + DY[i], x + DX[i], walkCount + 1]);
-			}
-		}
-	}
-};
+    if (this.#size === 0) this.#head = node;
+    else this.#tail.next = node;
 
-console.log(bfs());
+    this.#tail = node;
+
+    this.#size++;
+  }
+
+  shift() {
+    if (this.#size === 0) return -1;
+
+    const value = this.#head.value; // shift할 노드의 값 반환하기 위해 저장해주기
+
+    if (this.#size === 1) {
+      this.#head = null;
+      this.#tail = null;
+    } else this.#head = this.#head.next;
+
+    this.#size--;
+
+    return value;
+  }
+
+  getSize() {
+    return this.#size;
+  }
+}
+
+for (let i = 0; i < row; i++) {
+  const col = input[i].split('').map(Number);
+  map[i] = col;
+}
+
+function bfs([x, y]) {
+  const queue = new Queue();
+  queue.push([x, y, 1]); // 그때까지 걸린 걸음은 0
+
+  while (queue.getSize !== 0) {
+    const [x, y, count] = queue.shift();
+
+    if (y === row - 1 && x === col - 1) {
+      // 탈출위치
+      console.log(count);
+      break;
+    }
+
+    for (let i = 0; i < 4; i++) {
+      if (
+        ![-1, row].includes(y + DY[i]) &&
+        ![-1, col].includes(x + DX[i]) &&
+        !INVALID_TILE.includes(map[y + DY[i]][x + DX[i]])
+      ) {
+        queue.push([x + DX[i], y + DY[i], count + 1]);
+        map[y + DY[i]][x + DX[i]] = VISITED;
+      }
+    }
+  }
+}
+
+bfs([0, 0]);
+
+/*
+최단 거리를 찾아야함.  -> bfs
+
+bfs는 같은 레벨을 먼저 탐색해야함. -> 큐
+
+function bfs(시작위치) {
+    큐
+    시작위치를 큐에 넣는다.
+    
+    while(큐 길이가 0이 아니면 계속) {
+        맨앞 shift로 빼낸다.
+        이 값의 상하좌우를 push 한다.
+        
+}
+
+}
+    
+*/

143c484593b77594b1551edbcf9e5b1335f6e59b
PAKXE <64801796+pakxe@users.noreply.github.com>
2024-01-21 01:05:43 +0900

    [Bronze V] Title: A/B, Time: 176 ms, Memory: 9592 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.js
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -1,6 +1,3 @@
-const fs = require('fs');
-// let input = fs.readFileSync('e.txt').toString().split('\n');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-const [a, b] = input[0].split(' ').map(Number);
-
-console.log(a / b);
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n')[0];
+const [A, B] = input.split(' ').map(Number);
+console.log(A / B)

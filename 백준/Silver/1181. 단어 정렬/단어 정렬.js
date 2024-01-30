720b215ccd44a7ded68045070bbc673fd171c693
PAKXE <64801796+pakxe@users.noreply.github.com>
2024-01-30 22:05:23 +0900

    [Silver V] Title: 단어 정렬, Time: 228 ms, Memory: 19472 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.js
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -1,15 +1,19 @@
-const fs = require('fs');
-// let input = fs.readFileSync('e.txt').toString().split('\n');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-const wordCount = parseInt(input[0]);
-input.shift();
+/*
+중복을 제거하는건 set으로 한다. 
 
-const customSort = (a, b) => {
-	if (a.length > b.length) return 1;
-	if (a.length === b.length) {
-		return a < b ? -1 : 1;
-	}
-	if (a.length < b.length) return -1;
-};
+*/
 
-console.log([...new Set(input.sort(customSort))].join('\n'));
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'e.txt';
+const [N, ...input] = require('fs').readFileSync(filePath).toString().trim().split('\n');
+
+const set = new Set(input); // 중복 제거
+
+function strSort(a, b) {
+  if (a.length > b.length) return 1;
+  if (a.length < b.length) return -1;
+  if (a.length === b.length) {
+    return a > b ? 1 : -1;
+  }
+}
+
+console.log([...set].sort(strSort).join('\n'));

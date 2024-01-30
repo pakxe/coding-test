2855411689552ebeaa2837f41dd1f9ee48a9dad8
PAKXE <64801796+pakxe@users.noreply.github.com>
2024-01-30 21:44:30 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 9380 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.js
+++ 백준/Silver/9012. 괄호/괄호.js
@@ -1,27 +1,36 @@
-const fs = require('fs');
-// let input = fs.readFileSync('e.txt').toString().trim().split('\n');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-const LINE_COUNT = parseInt(input.shift());
+/*
 
-const OPEN = '(';
-const CLOSE = ')';
+*/
 
-const SUCCESS = 'YES';
-const FAIL = 'NO';
+const [VALID, INVALID] = ['YES', 'NO'];
+const [OPEN, CLOSE] = ['(', ')'];
 
-const isValidPS = (str) => {
-	const stack = [];
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'e.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 
-	for (let i = 0; i < str.length; i++) {
-		if (str[i] === OPEN) stack.push(str[i]);
-		if (str[i] === CLOSE) {
-			if (stack.length === 0) return false;
-			else stack.pop();
-		}
-	}
+const count = Number(input[0]);
+const answers = [];
 
-	return stack.length !== 0 ? false : true;
-};
+for (let i = 1; i <= count; i++) {
+  const str = input[i];
+  const stack = []; // 1로 채워지는 스택
+  let state = true;
 
-for (let i = 0; i < LINE_COUNT; i++)
-	console.log(isValidPS(input[i]) ? SUCCESS : FAIL);
+  for (let j = 0; j < str.length; j++) {
+    const cur = str[j];
+
+    // 비어있는데 close오면 실패, open오면 push
+    // 스택이 안비어있는데 close오면 pop, open오면 push
+
+    if (stack.length === 0 && cur === CLOSE) {
+      state = false;
+      break;
+    } else if (cur === OPEN) stack.push(1);
+    else if (stack.length !== 0 && cur === CLOSE) stack.pop();
+  }
+
+  if (!state || stack.length !== 0) answers.push(false);
+  else answers.push(true);
+}
+
+console.log(answers.map((state) => (state ? VALID : INVALID)).join('\n'));

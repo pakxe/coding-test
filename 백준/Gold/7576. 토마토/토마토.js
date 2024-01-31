afe6b5a425106a03c93c5e375dab1c834ac315d7
PAKXE <64801796+pakxe@users.noreply.github.com>
2024-01-31 16:53:06 +0900

    [Gold V] Title: 토마토, Time: 988 ms, Memory: 215620 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.js
+++ 백준/Gold/7576. 토마토/토마토.js
@@ -1,144 +1,136 @@
-const fs = require('fs');
-// let input = fs.readFileSync('e.txt').toString().split('\n');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-const [col, row] = input[0].split(' ').map(Number);
-input.shift();
-
 /*
-토마토 하나에서 bfs로 사방으로 전파된다. 
-for문으로 맵 전체를 돌아, 익은 토마토에 한에서 bfs를 돌린다. 이때 날짜도 함께 저장해 며칠이면 큐가 끝나는지 반환한다. (치킨거리처럼)
-그리고 이렇게 반환된 맥스 배열은 저장해둔다. 마지막에 최대 일수 출력시 사용한다. 
+익은 토마토 옆에 있으면 익지 않은 토마토가 하루 뒤에 익은 토마토가 된다. 
 
-그렇게 1을 다 돌았는데 0(익지않은 토마토)가 존재하는 경우 익지못하는 상황으로 판단. -1
+0이 모두 없어질 때까지의 최소 날짜를 출력하는 것.
+이미 다 익어있다면(0이 하나도 없다면) => 0
+다 돌았는데도 0이 존재한다면 => -1
 
-처음에 맵을 도는데 0이 없으면 0출력(모든 토마토 익)
 
+생각해야할게 있다. 토마토가 서로 다른 곳에 있는 경우 .. 그러면 어떻게 해아하지?
+
+답을 봐버렸다. 둘 다 큐에 넣으면 된다...
 */
-const map = input;
 
-const ADULT = 1;
-const CHILD = 0;
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'e.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  #head;
+  #tail;
+
+  constructor() {
+    this.size = 0;
+    this.#head = null;
+    this.#tail = null;
+  }
+
+  push(value) {
+    const node = new Node(value);
+
+    if (this.size === 0) this.#head = node;
+    else this.#tail.next = node;
+
+    this.#tail = node;
+
+    this.size += 1;
+  }
+
+  shift() {
+    if (this.size === 0) return -1;
+
+    const value = this.#head.value;
+
+    if (this.size === 1) {
+      this.#head = null;
+      this.#tail = null;
+    } else {
+      this.#head = this.#head.next;
+    }
+
+    this.size -= 1;
+
+    return value;
+  }
+
+  getSize() {
+    return this.size;
+  }
+}
+
+const BABY = 0;
+const MATURE = 1;
 const EMPTY = -1;
 
-const ALL_ADULT = 0;
-const ENABLE_ADULT = -1;
+// 맵 초기화
+const [W, H] = input[0].split(' ').map(Number);
+const map = new Array(H).fill();
 
 const DX = [0, 1, 0, -1];
 const DY = [1, 0, -1, 0];
 
-// function getRippingDays(start) {
-// 	const queue = [];
-// 	let needDays = 0;
+// 시작 좌표는 성숙 토마토 ``
+function bfs(startList) {
+  const queue = new Queue();
+  startList.forEach(([y, x]) => queue.push([y, x, 0]));
 
-// 	queue.push([...start, 0]); // 0일
+  let lastDay = 0;
 
-// 	while (queue.length > 0) {
-// 		const [y, x, days] = queue.shift();
+  while (queue.getSize()) {
+    const [y, x, day] = queue.shift();
+    lastDay = Math.max(day, lastDay);
 
-// 		if (map[y][x] !== EMPTY) {
-// 			map[y][x] = ADULT; // 익은 토마토로 바꾼다.
-// 			needDays = days + 1; // 하루 추가한다.
+    for (let i = 0; i < 4; i++) {
+      const [ny, nx] = [y + DY[i], x + DX[i]];
 
-// 			for (let i = 0; i < DX.length; i++) {
-// 				if (
-// 					![-1, col].includes(x + DX[i]) &&
-// 					![-1, row].includes(y + DY[i]) &&
-// 					map[y + DY[i]][x + DX[i]] === CHILD
-// 				) {
-// 					queue.push([y + DY[i], x + DX[i], days + 1]);
-// 					map[y + DY[i]][x + DX[i]] = ADULT; // 익었당
-// 				}
-// 			}
-// 		}
-// 	}
+      if (ny < 0 || ny >= H || nx < 0 || nx >= W) continue;
+      if (map[ny][nx] === BABY) {
+        map[ny][nx] = MATURE;
+        queue.push([ny, nx, day + 1]);
+      }
+    }
+  }
 
-// 	return needDays - 1;
-// }
-
-const days = new Array(row).fill().map(() => new Array(col).fill(0));
-let next = [];
-
-// start는 익은 토마토
-function getRippingDays(start) {
-	const [y, x] = start;
-
-	for (let i = 0; i < DX.length; i++) {
-		if (
-			![-1, col].includes(x + DX[i]) &&
-			![-1, row].includes(y + DY[i]) &&
-			map[y + DY[i]][x + DX[i]] === CHILD
-		) {
-			next.push([y + DY[i], x + DX[i]]);
-			map[y + DY[i]][x + DX[i]] = ADULT; // 익었당
-			days[y + DY[i]][x + DX[i]] = days[y][x] + 1; // 하루걸린다
-		}
-	}
+  return lastDay;
 }
 
-let needDays = [];
+let onlyMature = true;
+for (let i = 0; i < H; i++) {
+  map[i] = input[i + 1].split(' ').map(Number);
 
-// 익은 토마토 위치 받아오기
-function getAdultLocations() {
-	const adultLocations = [];
-
-	for (let y = 0; y < row; y++) {
-		for (let x = 0; x < col; x++) {
-			if (map[y][x] === ADULT) adultLocations.push([y, x]);
-		}
-	}
-
-	return adultLocations;
+  if (onlyMature && map[i].includes(BABY)) onlyMature = false;
 }
 
-// 맵으로 만들기
-for (let i = 0; i < input.length; i++) {
-	map[i] = input[i].split(' ').map(Number);
+if (onlyMature) {
+  console.log(0);
+  return;
 }
 
-// 이미 모두 성숙한 경우
-let state = true;
+const matureIndexList = [];
 
-for (let y = 0; y < row; y++) {
-	for (let x = 0; x < col; x++) {
-		if (map[y][x] === CHILD) {
-			state = false;
-			break;
-		}
-	}
+for (let y = 0; y < H; y++) {
+  for (let x = 0; x < W; x++) {
+    if (map[y][x] === MATURE) matureIndexList.push([y, x]);
+  }
 }
 
-if (state) {
-	console.log(ALL_ADULT);
-	return;
+const minDay = bfs(matureIndexList);
+
+let isHaveBaby = false;
+for (let y = 0; y < H; y++) {
+  for (let x = 0; x < W; x++) {
+    if (map[y][x] === BABY) {
+      isHaveBaby = true;
+      break;
+    }
+  }
 }
 
-let before = getAdultLocations().length; // 익은 토마토의 개수
-let after = 0;
-
-next = getAdultLocations(); // 처음 토마토 위치
-let queueIndex = 0;
-
-while (queueIndex < next.length) {
-	for (let i = queueIndex; i < next.length; i++) {
-		getRippingDays(next[i]);
-		queueIndex++;
-	}
-}
-
-for (let y = 0; y < row; y++) {
-	for (let x = 0; x < col; x++) {
-		if (map[y][x] === CHILD) {
-			console.log(ENABLE_ADULT);
-			return;
-		}
-	}
-}
-console.log(Math.max(...days.flat()));
-
-/**
- * 변화가 없을 때까지 반복한다.
- * 현재 맵을 찰칵해서 익은 토마토의 좌표를 배열로 얻어낸다.
- * 모든 익은 토마토 좌표에 대해서 한칸씩 전파한다.
- *
- * 변화의 감지는 맵에서 익은 토마토의 개수로 하자
- */
+if (isHaveBaby) console.log(-1);
+else console.log(minDay);

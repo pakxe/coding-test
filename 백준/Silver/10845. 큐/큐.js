c1d3b1674734dbb1a97990c4e71c276410a24479
PAKXE <64801796+pakxe@users.noreply.github.com>
2024-02-06 03:17:01 +0900

    [Silver IV] Title: 큐, Time: 196 ms, Memory: 12352 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.js
+++ 백준/Silver/10845. 큐/큐.js
@@ -1,92 +1,85 @@
-const fs = require('fs');
-//let input = fs.readFileSync('e.txt').toString().split('\n');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-const lineCount = parseInt(input[0]);
-input.shift();
-
 class Node {
-	constructor(value) {
-		this.value = value;
-		this.next = null;
-	}
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class Queue {
-	#size;
+  #head;
+  #tail;
 
-	constructor() {
-		this.head = null;
-		this.tail = null;
-		this.#size = 0;
-	}
+  constructor() {
+    this.size = 0;
+    this.#head = null;
+    this.#tail = null;
+  }
 
-	push(value) {
-		let node = new Node(value);
+  push(value) {
+    const node = new Node(value);
 
-		if (this.#size === 0) this.head = node;
-		else this.tail.next = node; // 테일이 가리키는 노드의 next를 새로 들어온 노드로 한다.
+    if (this.size === 0) this.#head = node;
+    else this.#tail.next = node;
 
-		this.tail = node;
-		this.#size++;
-	}
+    this.#tail = node;
 
-	getSize() {
-		return this.#size;
-	}
+    this.size += 1;
+  }
 
-	// shift
-	pop() {
-		if (this.#size === 0) return -1;
+  pop() {
+    if (this.size === 0) return -1;
 
-		const value = this.head.value; // shift할 노드의 값 반환하기 위해 저장해주기
+    const value = this.#head.value;
 
-		this.#size--;
-		if (this.#size === 0) {
-			this.head = null;
-			this.tail = null;
-		} else this.head = this.head.next;
+    if (this.size === 1) {
+      this.#head = null;
+      this.#tail = null;
+    } else this.#head = this.#head.next;
 
-		return value;
-	}
+    this.size -= 1;
 
-	empty() {
-		return this.#size === 0 ? 1 : 0;
-	}
+    return value;
+  }
 
-	front() {
-		if (this.#size === 0) return -1;
-		return this.head.value;
-	}
+  isEmpty() {
+    return this.size === 0 ? 1 : 0;
+  }
 
-	back() {
-		if (this.#size === 0) return -1;
-		return this.tail.value;
-	}
+  front() {
+    if (this.size === 0) return -1;
+
+    return this.#head.value;
+  }
+
+  back() {
+    if (this.size === 0) return -1;
+
+    return this.#tail.value;
+  }
 }
+
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'e.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const [PUSH, FRONT, SIZE, BACK, POP, EMPTY] = ['push', 'front', 'size', 'back', 'pop', 'empty'];
 
 const queue = new Queue();
-const result = [];
+const answers = [];
 
-const [PUSH, FRONT, BACK, SIZE, EMPTY, POP] = [
-	'push',
-	'front',
-	'back',
-	'size',
-	'empty',
-	'pop',
-];
+for (let i = 1; i <= n; i++) {
+  const command = input[i].split(' ');
+  const order = command[0];
 
-for (let i = 0; i < lineCount; i++) {
-	const command = input[i].split(' ');
-
-	if (command[0] === PUSH) {
-		queue.push(command[1]);
-	}
-	if (command[0] === FRONT) result.push(queue.front());
-	if (command[0] === BACK) result.push(queue.back());
-	if (command[0] === SIZE) result.push(queue.getSize());
-	if (command[0] === POP) result.push(queue.pop());
-	if (command[0] === EMPTY) result.push(queue.empty());
+  if (command.length === 1) {
+    if (order === FRONT) answers.push(queue.front());
+    if (order === BACK) answers.push(queue.back());
+    if (order === SIZE) answers.push(queue.size);
+    if (order === EMPTY) answers.push(queue.isEmpty());
+    if (order === POP) answers.push(queue.pop());
+  } else {
+    if (order === PUSH) queue.push(Number(command[1]));
+  }
 }
 
-console.log(result.join('\n'));
+console.log(answers.join('\n'));

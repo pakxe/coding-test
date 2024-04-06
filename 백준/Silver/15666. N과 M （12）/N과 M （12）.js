d9bf19f22fae1ce8157963ceaa85fa35a8c5f71c
Pakxe <64801796+pakxe@users.noreply.github.com>
2024-04-06 21:06:23 +0900

    [Silver II] Title: N과 M (12), Time: 176 ms, Memory: 10408 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.js
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.js
@@ -1,40 +1,36 @@
-const fs = require('fs');
-// const input = fs.readFileSync('e.txt').toString().trim().split('\n');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-
-const [N, M] = input[0].split(' ').map(Number);
-const arr = input[1]
-	.split(' ')
-	.map(Number)
-	.sort((a, b) => a - b);
-
-// 중복 x 순서 상관 있음. M 길이의 수열
-// 사전 순으로 증가해야한다는 것은 순서가 상관 없다는 의미다.
-
 /*
-이어서 읽어야할 인덱스를 준다. 그리고 현재 길이
+오름차순. idx부터 시작
+중복 한번만 허용
 */
-const getSequence = (arr, lest) => {
-	// 길이를 넘으면 멈춘다.
-	if (lest === 1) return [...new Set(arr)].map((el) => [el]);
 
-	const result = [];
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'e.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 
-	[...new Set(arr)].forEach((cur, index) => {
-		const findIndex = arr.indexOf(cur);
-		const rest = arr.slice(findIndex);
+const [count, maxLength] = input[0].split(' ').map(Number);
+const numbers = input[1]
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b);
 
-		const sequence = getSequence(rest, lest - 1);
-		result.push(...sequence.map((el) => [cur, ...el]));
-	});
+const temp = [];
+const resultList = [];
+function bt(idx) {
+  if (temp.length === maxLength) {
+    resultList.push(temp.join(' '));
+    return;
+  }
 
-	return result;
-};
+  let value = null;
+  for (let i = idx; i < count; i++) {
+    if (value === numbers[i]) continue;
+    value = numbers[i];
 
-// const arr = new Array(N).fill().map((_, i) => i + 1);
+    temp.push(numbers[i]);
+    bt(i);
+    temp.pop();
+  }
+}
 
-console.log(
-	getSequence(arr, M)
-		.map((arr) => arr.join(' '))
-		.join('\n')
-);
+bt(0);
+
+console.log(resultList.join('\n'));

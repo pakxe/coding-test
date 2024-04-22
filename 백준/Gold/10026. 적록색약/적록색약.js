461728f0f91dbfdf627ddd201ccc945ea8a05cb2
Pakxe <64801796+pakxe@users.noreply.github.com>
2024-04-22 17:45:43 +0900

    [Gold V] Title: 적록색약, Time: 348 ms, Memory: 15784 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.js
+++ 백준/Gold/10026. 적록색약/적록색약.js
@@ -1,86 +1,140 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'e.txt';
-const input = require('fs')
-	.readFileSync(filePath)
-	.toString()
-	.trim()
-	.split('\n');
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 
-const row = parseInt(input.shift());
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
-const map = new Array(row).fill();
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
 
-for (let i = 0; i < row; i++) {
-	map[i] = input[i].split('');
+  push(value) {
+    const node = new Node(value);
+
+    if (this.size === 0) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+
+    this.size += 1;
+
+    return;
+  }
+
+  shift() {
+    if (this.size === 0) return -1;
+
+    const value = this.head.value;
+
+    if (this.size === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+    }
+
+    this.size -= 1;
+
+    return value;
+  }
+}
+
+// 적록색약은 빨강과 초록을 같은 색으로 본다.
+
+const SIZE = parseInt(input[0]);
+const map = new Array(SIZE).fill();
+
+for (let i = 0; i < SIZE; i++) {
+  map[i] = input[i + 1].split('');
 }
 
 const DX = [0, 1, 0, -1];
 const DY = [1, 0, -1, 0];
+function normal(sy, sx, visited) {
+  const targetValue = map[sy][sx];
+  const queue = new Queue();
+  visited[sy][sx] = true;
 
-const bfs = (start, visited) => {
-	const color = map[start[0]][start[1]];
-	const queue = [];
+  queue.push([sy, sx]);
 
-	queue.push(start);
+  while (queue.size !== 0) {
+    const [y, x] = queue.shift();
 
-	while (queue.length) {
-		const [y, x] = queue.shift();
+    for (let i = 0; i < DX.length; i++) {
+      const [ny, nx] = [y + DY[i], x + DX[i]];
 
-		for (let i = 0; i < DX.length; i++) {
-			const [ny, nx] = [y + DY[i], x + DX[i]];
+      if (ny < 0 || ny >= SIZE || nx < 0 || nx >= SIZE) continue;
+      if (visited[ny][nx]) continue;
+      if (targetValue !== map[ny][nx]) continue;
 
-			if (
-				![-1, row].includes(ny) &&
-				![-1, row].includes(nx) &&
-				map[ny][nx] === color &&
-				!visited[ny][nx]
-			) {
-				visited[ny][nx] = true;
-				queue.push([ny, nx]);
-			}
-		}
-	}
-};
-
-const specialBfs = (start, visited) => {
-	const color = map[start[0]][start[1]] === 'B' ? 'B' : 'RG';
-	const queue = [];
-
-	queue.push(start);
-
-	while (queue.length) {
-		const [y, x] = queue.shift();
-
-		for (let i = 0; i < DX.length; i++) {
-			const [ny, nx] = [y + DY[i], x + DX[i]];
-
-			if (
-				![-1, row].includes(ny) &&
-				![-1, row].includes(nx) &&
-				color.includes(map[ny][nx]) &&
-				!visited[ny][nx]
-			) {
-				visited[ny][nx] = true;
-				queue.push([ny, nx]);
-			}
-		}
-	}
-};
-
-let result = [0, 0];
-for (let i = 0; i < 2; i++) {
-	let visited = new Array(row).fill().map(() => new Array(row).fill(false));
-
-	for (let y = 0; y < row; y++) {
-		for (let x = 0; x < row; x++) {
-			if (!visited[y][x]) {
-				visited[y][x] = true;
-				if (i === 1) specialBfs([y, x], visited);
-				else bfs([y, x], visited);
-
-				result[i]++;
-			}
-		}
-	}
+      visited[ny][nx] = true;
+      queue.push([ny, nx]);
+    }
+  }
 }
 
-console.log(result.join(' '));
+// 같은 영역으로 체크할 것
+function checkRedGreenColor(ny, nx, targetValue) {
+  const curValue = map[ny][nx];
+
+  if (targetValue === 'B' && curValue === 'B') return true;
+  if (targetValue !== 'B' && (curValue === 'G' || curValue === 'R')) return true;
+  return false;
+}
+
+function redGreenColorBlindness(sy, sx, visited) {
+  const targetValue = map[sy][sx];
+  const queue = new Queue();
+  visited[sy][sx] = true;
+
+  queue.push([sy, sx]);
+
+  while (queue.size !== 0) {
+    const [y, x] = queue.shift();
+
+    for (let i = 0; i < DX.length; i++) {
+      const [ny, nx] = [y + DY[i], x + DX[i]];
+
+      if (ny < 0 || ny >= SIZE || nx < 0 || nx >= SIZE) continue;
+      if (visited[ny][nx]) continue;
+      if (checkRedGreenColor(ny, nx, targetValue)) {
+        visited[ny][nx] = true;
+        queue.push([ny, nx]);
+      }
+    }
+  }
+}
+
+let normalAreaCount = 0;
+const normalVisited = new Array(SIZE).fill().map(() => new Array(SIZE).fill(false));
+for (let y = 0; y < SIZE; y++) {
+  for (let x = 0; x < SIZE; x++) {
+    if (!normalVisited[y][x]) {
+      normal(y, x, normalVisited);
+      normalAreaCount += 1;
+    }
+  }
+}
+
+let specialAreaCount = 0;
+const specialVisited = new Array(SIZE).fill().map(() => new Array(SIZE).fill(false));
+for (let y = 0; y < SIZE; y++) {
+  for (let x = 0; x < SIZE; x++) {
+    if (!specialVisited[y][x]) {
+      redGreenColorBlindness(y, x, specialVisited);
+      specialAreaCount += 1;
+    }
+  }
+}
+
+console.log(normalAreaCount, specialAreaCount);

63f81302860d6c23294116d1a8bbbbe2082fed22
Pakxe <64801796+pakxe@users.noreply.github.com>
2024-05-04 05:00:10 +0900

    [Bronze V] Title: 검증수, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.js
+++ 백준/Bronze/2475. 검증수/검증수.js
@@ -1,10 +1,7 @@
-const filePath = process.platform === 'linux' ? '/dev/stdin' : 'e.txt';
-const input = require('fs')
-	.readFileSync(filePath)
-	.toString()
-	.trim()
-	.split('\n');
-const arr = input.shift().split(' ').map(Number);
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const sum = arr.reduce((sum, cur) => sum + Math.pow(cur, 2), 0);
-console.log(sum % 10);
+
+const nums = input[0].split(' ').map(Number);
+
+const sqrtedNums = nums.map(num => num ** 2 );
+console.log(sqrtedNums.reduce((sum, n) => sum + n, 0) % 10)

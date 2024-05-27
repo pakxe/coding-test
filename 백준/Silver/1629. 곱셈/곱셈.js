31481ae180b0440cf756c04ce6407de727c81eaa
Pakxe <64801796+pakxe@users.noreply.github.com>
2024-05-27 18:16:42 +0900

    [Silver I] Title: 곱셈, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.js
+++ 백준/Silver/1629. 곱셈/곱셈.js
@@ -1,28 +1,23 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'e.txt';
-const input = require('fs').readFileSync(filePath).toString().split('\n');
-const [A, B, C] = input.shift().split(' ').map(BigInt);
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+
+const [a, b, c] = input[0].split(' ').map(BigInt);
+
 /*
-짝수와 홀수인 경우를 생각해야한다.
-
-짝수: A^B = A^(B/2) * A^(B/2)
-A^B % C = (A^(B/2) * A^(B/2)) % C
-        = (A^(B/2) % C * A^(B/2) % C) % C;
-        = f(B/2)^2 % C
-
-홀수: A ^ B = (A^(B/2) * A^(B/2) * A
-A ^ B % C = (A^(B/2) * A^(B/2) * A) % C
-          = ((A^(B/2) * A^(B/2)) % C * A % C) % C
-          = (f(B/2)^2 % C * A % C) % C
-          = f(B/2)^2 * A % C
+재귀형태로 분할 정복하는 함수를 만든다.
 */
 
-// 모든 인자는 BigInt
-function modular(a, b, c) {
-  if (b <= BigInt(1)) return a % c;
-
-  // 짝수인 경우
-  if (b % BigInt(2) === BigInt(0)) return modular(a, b / BigInt(2), c) ** BigInt(2) % c;
-  else return (modular(a, (b - BigInt(1)) / BigInt(2), c) ** BigInt(2) * A) % c;
+const getCalc = (n) => {
+    if(n === BigInt(1)) return a % c;
+    
+    if(n % BigInt(2) === BigInt(0)) { // 짝수
+        const v = getCalc(n / BigInt(2));
+        return BigInt((v % c * v % c) % c);
+    } else {
+        const v = getCalc((n - BigInt(1)) / BigInt(2));
+        return BigInt((v % c * v % c * a % c) % c);
+    }
 }
 
-console.log(modular(A, B, C).toString());
+console.log(getCalc(b).toString())
+

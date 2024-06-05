06929ca70053266303c9f68a01a5422764c1d4a7
Pakxe <64801796+pakxe@users.noreply.github.com>
2024-06-06 01:50:44 +0900

    [Silver IV] Title: 수 찾기, Time: 300 ms, Memory: 46296 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.js
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -1,40 +1,26 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+/**
+n*n인 문제.
 
-const FIND = 1;
-const NOTFOUND = 0;
+정렬해서 이분탐색하는 방법으로 풀 수 있을 것 같다.
 
-const arr = input[1]
-	.split(' ')
-	.map(Number)
-	.sort((a, b) => a - b);
-
-const result = [];
-
-const binarySearch = (start, end, target) => {
-	if (start > end) return NOTFOUND; // 못찾음
-
-	let middle = Math.floor((end + start) / 2); // 중간 좌표
-
-	if (target === arr[middle]) return FIND;
-	if (target < arr[middle]) return binarySearch(start, middle - 1, target);
-	if (target > arr[middle]) return binarySearch(middle + 1, end, target);
-};
-
-const needFindArr = input[3].split(' ').map(Number);
-
-for (let i = 0; i < parseInt(input[2]); i++) {
-	result.push(binarySearch(0, arr.length - 1, needFindArr[i]));
-}
-
-console.log(result.join('\n'));
-
-/*
-시간 초과가 관건인 문제같다. 
-
-분할 서치가 필요할 것 같다. 중복은 명시가 안되어있으니 있다고 가정하자. 
-
-입력받은 수를 배열에 추가하고. 찾아야하는 수로 순회한다. 
-
-find함수 필요
+해시맵은 잘 모르겠는데 그럼 메모리 터지지 않나? => 이 방법은 찾아보는게 좋을
 */
+
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'e.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+
+const n = parseInt(input[0]);
+const nNums = input[1].split(' ').map(Number);
+const m = parseInt(input[2]);;
+const mNums = input[3].split(' ').map(Number);
+
+const set = new Set(nNums);
+
+const resultList = [];
+
+mNums.forEach(num => {
+    if(set.has(num)) resultList.push(1);
+    else resultList.push(0);
+})
+
+console.log(resultList.join('\n'))

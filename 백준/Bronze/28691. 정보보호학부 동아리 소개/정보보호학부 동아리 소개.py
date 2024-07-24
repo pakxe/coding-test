b5d8a7598df42b998ad4d6cfd5576502ab3d2d38
Pakxe <64801796+pakxe@users.noreply.github.com>
2024-07-25 02:25:33 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,13 @@
+firstName = input()
+
+if firstName == 'M':
+    print('MatKor')
+elif firstName == 'W':
+    print('WiCys')
+elif firstName == 'C':
+    print('CyKor')
+elif firstName == 'A':
+    print('AlKor')
+elif firstName == '$':
+    print('$clear')
+    

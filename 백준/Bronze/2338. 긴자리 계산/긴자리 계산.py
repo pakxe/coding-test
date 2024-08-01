308138da434aa6d5966de22a75638348f6c71ccb
Pakxe <64801796+pakxe@users.noreply.github.com>
2024-08-02 02:33:34 +0900

    [Bronze V] Title: 긴자리 계산, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.py
@@ -0,0 +1,10 @@
+import sys
+
+# Reading two lines of input
+a = int(sys.stdin.readline().strip())
+b = int(sys.stdin.readline().strip())
+
+# Perform calculations
+print(a + b)
+print(a - b)
+print(a * b)

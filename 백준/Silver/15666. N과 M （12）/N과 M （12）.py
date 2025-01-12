3dbce714c8cf2952b99e46544f07a6f54349e8a0
Pakxe <64801796+pakxe@users.noreply.github.com>
2025-01-13 00:21:38 +0900

    [Silver II] Title: N과 M (12), Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,25 @@
+n, m = map(int, input().split())
+lst = list(map(int, input().split()))
+
+lst.sort()
+
+temp = []
+def comb(curIdx):
+    if len(temp) == m:
+        print(' '.join(map(str, temp)))
+        return
+    
+    prev = None
+    for i in range(curIdx, n):
+        cur = lst[i]
+        
+        if cur == prev:
+            continue
+        
+        prev = cur
+        
+        temp.append(cur)
+        comb(i)
+        temp.pop()
+    
+comb(0)

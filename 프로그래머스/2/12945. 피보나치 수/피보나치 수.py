35246b77981aa38dddea29452fe9e174080b8d61
Pakxe <64801796+pakxe@users.noreply.github.com>
2025-09-18 13:51:31 +0900

    [level 2] Title: 피보나치 수, Time: 568.27 ms, Memory: 455 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,14 @@
+'''
+메모리에이션
+
+'''
+
+def solution(n):
+    fibo = [0 for _ in range(n + 1)]
+    fibo[0] = 0
+    fibo[1] = 1
+    
+    for i in range(2, n + 1):
+        fibo[i] = fibo[i - 1] + fibo[i - 2]
+        
+    return fibo[n] % 1234567
